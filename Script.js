e0956540d98116988bb11ecf88499ff437c714c2
MaxwellDeLara <127850040+MaxwellDeLara@users.noreply.github.com>
2023-10-02 10:40:23 -0300

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,12 +1,12 @@
 function funcaoResposta(){
-    var respostaCorreta = "correta"
+    var respostaCorreta = "correta";
     var respostaUsuario = document.querySelector('input[name="resposta"]:checked');
     if(respostaUsuario){
-        if(respostaUsuario.value === retaCorreta){
-            document.getElementById("reo").textContent = "Correto";
+        if(respostaUsuario.value === respostaCorreta){
+            document.getElementById("retorno").textContent = "Correto!";
         }
         else{
-            document.getElementById("retorno").textContent = "";
+            document.getElementById("retorno").textContent = "Errado!";
         }
     }
     else{
