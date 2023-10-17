aeae41403293261c7b837b0483142c3585128a22
MaxwellDeLara <127850040+MaxwellDeLara@users.noreply.github.com>
2023-10-17 10:45:48 -0300

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,3 +1,22 @@
+function mudar(objeto, imagem){
+    porta.src = imagem;
+}
+
+function mudar(objeto, imagem) {
+
+var elemento = document.getElementById(objeto);
+
+if (elemento) {
+elemento.style.opacity = 0;/* Define a opacidade para 0 para iniciar a transição */
+setTimeout(function() {
+elemento.src = imagem;/* Altera a imagem após um pequeno atraso */
+elemento.style.opacity = 1;/* Define a opacidade de volta para 1 para a transição suave */
+}, 300);/* Atraso de 500 milissegundos (0,5 segundos) */
+}
+else {
+console.error("Elemento não encontrado com o ID: " + objeto);
+}
+}
 function funcaoResposta(){
     var respostaCorreta = "correta";
     var respostaUsuario = document.querySelector('input[name="resposta"]:checked');
